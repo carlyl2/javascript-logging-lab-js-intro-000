508887b64177506939b5796828279a2d24b92b4c
Carly Lord <carly.lord@gmail.com>
2019-10-13 07:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.error('Uh oh,','something is seriously broken...','I\'m scared!')
 console.log('Logging is important for recording client behavior.')
-console.warn('Careful! He\'s off his meds!'
+console.warn('Careful! He\'s off his meds!')
+
